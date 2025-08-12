e1b0fea9f9fbc4020d2547b836982f777545631e
lopezmfede-del <lopezmfede@gmail.com>
2025-08-11 22:41:47 -0600

    Contador de veces que aparece caracter een cadena y retorno de codigo de éxito y cantidad

--- Tarea 1/tarea_1_example_solution.py
+++ Tarea 1/tarea_1_example_solution.py
@@ -13,4 +13,13 @@ def count_char(cadena, caracter):
     if not all (c.isalnum() for c in cadena):
         return ERROR_FORMATO_CADENA, None
     
-    #Tercero, 
+    #Tercero, verificar que 'caracter' sea un único carácter alfanumérico
+    if not (isinstance(caracter, str) and len(caracter) == 1 and caracter.isalnum()):
+        return ERROR_CARACTER, None
+    
+    #Cuarto, contar cantidad de veces que aparece 'caracter' en 'cadena'
+    cantidad = cadena.count(caracter)
+    
+    #Finalmente, retornar el código de éxito y la cantidad
+    return EXITO, cantidad
+    
