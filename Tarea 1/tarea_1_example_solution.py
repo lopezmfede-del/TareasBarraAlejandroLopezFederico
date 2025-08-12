ee9bf2ef256fcb68d08bf0b74572d4f6ae1586f1
lopezmfede-del <lopezmfede@gmail.com>
2025-08-11 22:36:33 -0600

    Condición que verifica que la cadena solo tiene numeros y letras

--- Tarea 1/tarea_1_example_solution.py
+++ Tarea 1/tarea_1_example_solution.py
@@ -8,3 +8,9 @@ def count_char(cadena, caracter):
     #Primero, verificar que 'cadena' es un string
     if not isinstance(cadena, str):
         return ERROR_TIPO_CADENA, None
+    
+    #Segundo, verificar que la cadena solo tenga numeros y letras
+    if not all (c.isalnum() for c in cadena):
+        return ERROR_FORMATO_CADENA, None
+    
+    #Tercero, 
