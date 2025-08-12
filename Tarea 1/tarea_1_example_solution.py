d9da6513fb0d7c704f418b807869824260fc062a
lopezmfede-del <lopezmfede@gmail.com>
2025-08-11 22:27:33 -0600

    Definición de variables de error y de éxito

---
+++ Tarea 1/tarea_1_example_solution.py
@@ -0,0 +1,6 @@
+def count_char(cadena, caracter):
+    #Definición de codigos de exito y erores
+    EXITO = 0
+    ERROR_TIPO_CADENA = -100
+    ERROR_FORMATO_CADENA = -200
+    ERROR_CARACTER = -300
