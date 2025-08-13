b72c496e0ad29fb30c59bedd488d75b222367e26
Barracouldve <alejandrobarramo@gmail.com>
2025-08-13 16:52:29 -0600

    Correccion de errores adicionales con cambio de nombre de variable (Caracter a car)

--- Tarea 1/tarea_1_example_solution.py
+++ Tarea 1/tarea_1_example_solution.py
@@ -1,4 +1,4 @@
-def count_char(cadena, caracter):
+def count_char(cadena, car):
     # Definición de codigos de exito y erores
     EXITO = 0
     ERROR_TIPO_CADENA = -100
@@ -10,15 +10,15 @@ def count_char(cadena, caracter):
         return ERROR_TIPO_CADENA, None
 
     # Segundo, verificar que la cadena solo tenga numeros y letras
-    if not all (c.isalnum() for c in cadena):
+    if not all(c.isalnum() for c in cadena):
         return ERROR_FORMATO_CADENA, None
 
     # Tercero, verificar que 'caracter' sea un único carácter alfanumérico
-    if not (isinstance(caracter, str) and len(caracter) == 1 and caracter.isalnum()):
+    if not (isinstance(car, str) and len(car) == 1 and car.isalnum()):
         return ERROR_CARACTER, None
 
     # Cuarto, contar cantidad de veces que aparece 'caracter' en 'cadena'
-    cantidad = cadena.count(caracter)
+    cantidad = cadena.count(car)
 
     # Finalmente, retornar el código de éxito y la cantidad
     return EXITO, cantidad
