2424f7c524e6c992b2279957e44d9b7a2651b221
Barracouldve <alejandrobarramo@gmail.com>
2025-08-13 16:45:33 -0600

    Correccion de error "blank line contains whitespace"

--- Tarea 1/tarea_1_example_solution.py
+++ Tarea 1/tarea_1_example_solution.py
@@ -8,18 +8,18 @@ def count_char(cadena, caracter):
     # Primero, verificar que 'cadena' es un string
     if not isinstance(cadena, str):
         return ERROR_TIPO_CADENA, None
-    
+
     # Segundo, verificar que la cadena solo tenga numeros y letras
     if not all (c.isalnum() for c in cadena):
         return ERROR_FORMATO_CADENA, None
-    
+
     # Tercero, verificar que 'caracter' sea un único carácter alfanumérico
     if not (isinstance(caracter, str) and len(caracter) == 1 and caracter.isalnum()):
         return ERROR_CARACTER, None
-    
+
     # Cuarto, contar cantidad de veces que aparece 'caracter' en 'cadena'
     cantidad = cadena.count(caracter)
-    
+
     # Finalmente, retornar el código de éxito y la cantidad
     return EXITO, cantidad
 
@@ -33,7 +33,7 @@ def multiplo_2(base,multiplo):
     # Multiplo debe estar en el conjunto permitido
     if multiplo not in (1, 2, 4, 8, 16):
         return ERROR_MULTIPLO, None
-    
+
     return EXITO, base*multiplo
-    
+
 
