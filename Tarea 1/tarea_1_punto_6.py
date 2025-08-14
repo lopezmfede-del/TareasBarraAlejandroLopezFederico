6f14ce5bdd7bf8c23e63ee911318600a15a40f64
Barracouldve <alejandrobarramo@gmail.com>
2025-08-13 19:27:17 -0600

    Punto7Tarea1

--- Tarea 1/tarea_1_punto_6.py
+++ Tarea 1/tarea_1_punto_6.py
@@ -1,18 +1,18 @@
 def count_char(cadena, caracter):
-    #Definición de codigos de exito y erores
+    # Definición de codigos de exito y erores
     EXITO = 0
     ERROR_TIPO_CADENA = -100
     ERROR_FORMATO_CADENA = -200
     ERROR_CARACTER = -300
 
     # Primero, verificar que 'cadena' es un string
-    if not isinstance(cadena,str):
+    if not isinstance(cadena, str):
         return ERROR_TIPO_CADENA, None
 
     # Segundo, verificar que la cadena solo tenga numeros y letras
     if not all(c.isalnum() for c in cadena):
         return ERROR_FORMATO_CADENA, None
-    
+
     # Tercero, verificar que 'caracter' sea un único carácter alfanumérico
     if not (
         isinstance(caracter, str)
